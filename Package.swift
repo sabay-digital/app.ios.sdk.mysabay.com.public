bffa02489819081e84be7653e4c24cd676d7591b
Lay Channara <lay.channara@sabay.com>
2021-03-30 16:26:02 +0700

    Update SPM

--- Package.swift
+++ Package.swift
@@ -6,29 +6,29 @@ import PackageDescription
 let package = Package(
 	name: "MySabaySdk",
 	products: [
-		.library(name: "MySabaySdk", targets: ["MySabaySdkFramework", "MySabaySdkDependencies"])
+		.library(name: "MySabaySdk", targets: ["MySabaySdk", "Dependencies"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/evgenyneu/keychain-swift.git", from: "19.0.0"),
-		.package(url: "https://github.com/marmelroy/PhoneNumberKit", from: "3.3.1"),
-		.package(url: "https://github.com/rexmas/JSONValue.git", from: "7.0.0"),
-		.package(url: "https://github.com/facebook/facebook-ios-sdk", from: "8.2.0"),
-		.package(url: "https://github.com/apollographql/apollo-ios.git", from: "0.37.0"),
-		.package(url: "https://github.com/bizz84/SwiftyStoreKit.git", from: "0.16.3"),
-		.package(url: "https://github.com/ReSwift/ReSwift.git", from: "6.0.0"),
-		.package(url: "https://github.com/matomo-org/matomo-sdk-ios.git", from: "7.4.1"),
-		.package(url: "https://github.com/onevcat/Kingfisher.git", from: "6.1.1")
+		.package(name: "KeychainSwift", url: "https://github.com/evgenyneu/keychain-swift.git", from: "19.0.0"),
+		.package(name: "PhoneNumberKit", url: "https://github.com/marmelroy/PhoneNumberKit", from: "3.3.1"),
+		.package(name: "JSONValue", url: "https://github.com/rexmas/JSONValue.git", from: "7.0.0"),
+		.package(name: "Facebook", url: "https://github.com/facebook/facebook-ios-sdk", from: "8.2.0"),
+		.package(name: "Apollo", url: "https://github.com/apollographql/apollo-ios.git", from: "0.37.0"),
+		.package(name: "SwiftyStoreKit", url: "https://github.com/bizz84/SwiftyStoreKit.git", from: "0.16.3"),
+		.package(name: "ReSwift", url: "https://github.com/ReSwift/ReSwift.git", from: "6.0.0"),
+		.package(name: "MatomoTracker", url: "https://github.com/matomo-org/matomo-sdk-ios.git", from: "7.4.1"),
+		.package(name: "Kingfisher", url: "https://github.com/onevcat/Kingfisher.git", from: "6.1.1")
 	],
 	targets: [
-		.binaryTarget(name: "MySabaySdkFramework", path: "./Sources/MySabaySdkFramework/MySabaySdk.xcframework"),
-		.target(name: "MySabaySdkDependencies", dependencies: ["keychain-swift",
+		.binaryTarget(name: "MySabaySdk", path: "./Sources/MySabaySdk.xcframework"),
+		.target(name: "Dependencies", dependencies: ["KeychainSwift",
 															   "PhoneNumberKit",
 															   "JSONValue",
-															   "facebook-ios-sdk",
-															   "apollo-ios",
+															   "Facebook",
+															   "Apollo",
 															   "SwiftyStoreKit",
 															   "ReSwift",
-															   "matomo-sdk-ios",
-															   "Kingfisher"], path: "./Sources/MySabaySdkDependencies")
+															   "MatomoTracker",
+															   "Kingfisher"], path: "./Sources/Dependencies")
 	]
 )

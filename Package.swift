60a272cb03ca764c5fca3a577be1c884be9b63ab
Lay Channara <lay.channara@sabay.com>
2021-03-30 09:36:29 +0700

    Update checksum of zip

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
 		.binaryTarget(
 			name: "MySabaySdk",
 			url: "https://git.sabay.com/mysabay/sdk/app.ios.sdk.mysabay.com.public/raw/1.0.31/MySabaySdk.zip",
-			checksum: "179e3c4129c1350b66892f6e914563922afb862a45869c843c8290df6faaf565"
+			checksum: "1616297bcf20f507dde9e2f9c02037c62346fe573e03f7b8ab026c5058b35acc"
 		),
     ]
 )

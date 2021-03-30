8fec91a70072bf3de03b8c17008cb21a254cda38
Lay Channara <lay.channara@sabay.com>
2021-03-30 15:33:17 +0700

    wip: Package.swift

--- Package.swift
+++ Package.swift
@@ -5,31 +5,17 @@ import PackageDescription
 
 let package = Package(
 	name: "MySabaySdk",
-	products: [
-		.library(name: "MySabaySdk", targets: ["MySabaySdk", "Dependencies"]),
-	],
-	dependencies: [
-		.package(url: "https://github.com/evgenyneu/keychain-swift.git", from: "19.0.0"),
-		.package(url: "https://github.com/marmelroy/PhoneNumberKit", from: "3.3.1"),
-		.package(url: "https://github.com/rexmas/JSONValue.git", from: "7.0.0"),
-		.package(url: "https://github.com/facebook/facebook-ios-sdk", from: "8.2.0"),
-		.package(url: "https://github.com/apollographql/apollo-ios.git", from: "0.37.0"),
-		.package(url: "https://github.com/bizz84/SwiftyStoreKit.git", from: "0.16.3"),
-		.package(url: "https://github.com/ReSwift/ReSwift.git", from: "6.0.0"),
-		.package(url: "https://github.com/matomo-org/matomo-sdk-ios.git", from: "7.4.1"),
-		.package(url: "https://github.com/onevcat/Kingfisher.git", from: "6.1.1"),
-	],
-	targets: [
-		.binaryTarget(name: "MySabaySdk", path: "./Sources/MySabaySdk.xcframework"),
-		.target(name: "Dependencies", dependencies: [
-					"JSONValue",
-					"Kingfisher",
-					"PhoneNumberKit",
-					"ReSwift",
-					"SwiftyStoreKit",
-					"apollo-ios",
-					"facebook-ios-sdk",
-					"keychain-swift",
-					"matomo-sdk-ios"])
-	]
+	products: [.library(name: "MySabaySdk", targets: ["MySabaySdk"])],
+	dependencies: [],
+	targets: [.binaryTarget(name: "MySabaySdk", path: "./Sources/MySabaySdk.xcframework")]
 )
+
+//.package(url: "https://github.com/evgenyneu/keychain-swift.git", from: "19.0.0"),
+//.package(url: "https://github.com/marmelroy/PhoneNumberKit", from: "3.3.1"),
+//.package(url: "https://github.com/rexmas/JSONValue.git", from: "7.0.0"),
+//.package(url: "https://github.com/facebook/facebook-ios-sdk", from: "8.2.0"),
+//.package(url: "https://github.com/apollographql/apollo-ios.git", from: "0.37.0"),
+//.package(url: "https://github.com/bizz84/SwiftyStoreKit.git", from: "0.16.3"),
+//.package(url: "https://github.com/ReSwift/ReSwift.git", from: "6.0.0"),
+//.package(url: "https://github.com/matomo-org/matomo-sdk-ios.git", from: "7.4.1"),
+//.package(url: "https://github.com/onevcat/Kingfisher.git", from: "6.1.1"),

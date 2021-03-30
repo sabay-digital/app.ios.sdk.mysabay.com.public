f2d05fa4eeef91513cc18aabe08fd7775b319ab1
Lay Channara <lay.channara@sabay.com>
2021-03-30 16:05:33 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,17 +5,30 @@ import PackageDescription
 
 let package = Package(
 	name: "MySabaySdk",
-	products: [.library(name: "MySabaySdk", targets: ["MySabaySdk"])],
-	dependencies: [],
-	targets: [.binaryTarget(name: "MySabaySdk", path: "./Sources/MySabaySdk.xcframework")]
+	products: [
+		.library(name: "MySabaySdk", targets: ["MySabaySdkFramework", "MySabaySdkDependencies"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/evgenyneu/keychain-swift.git", from: "19.0.0"),
+		.package(url: "https://github.com/marmelroy/PhoneNumberKit", from: "3.3.1"),
+		.package(url: "https://github.com/rexmas/JSONValue.git", from: "7.0.0"),
+		.package(url: "https://github.com/facebook/facebook-ios-sdk", from: "8.2.0"),
+		.package(url: "https://github.com/apollographql/apollo-ios.git", from: "0.37.0"),
+		.package(url: "https://github.com/bizz84/SwiftyStoreKit.git", from: "0.16.3"),
+		.package(url: "https://github.com/ReSwift/ReSwift.git", from: "6.0.0"),
+		.package(url: "https://github.com/matomo-org/matomo-sdk-ios.git", from: "7.4.1"),
+		.package(url: "https://github.com/onevcat/Kingfisher.git", from: "6.1.1")
+	],
+	targets: [
+		.binaryTarget(name: "MySabaySdkFramework", path: "./Sources/MySabaySdkFramework/MySabaySdk.xcframework"),
+		.target(name: "MySabaySdkDependencies", dependencies: ["keychain-swift",
+															   "PhoneNumberKit",
+															   "JSONValue",
+															   "facebook-ios-sdk",
+															   "apollo-ios",
+															   "SwiftyStoreKit",
+															   "ReSwift",
+															   "matomo-sdk-ios",
+															   "Kingfisher"], path: "./Sources/MySabaySdkDependencies")
+	]
 )
-
-//.package(url: "https://github.com/evgenyneu/keychain-swift.git", from: "19.0.0"),
-//.package(url: "https://github.com/marmelroy/PhoneNumberKit", from: "3.3.1"),
-//.package(url: "https://github.com/rexmas/JSONValue.git", from: "7.0.0"),
-//.package(url: "https://github.com/facebook/facebook-ios-sdk", from: "8.2.0"),
-//.package(url: "https://github.com/apollographql/apollo-ios.git", from: "0.37.0"),
-//.package(url: "https://github.com/bizz84/SwiftyStoreKit.git", from: "0.16.3"),
-//.package(url: "https://github.com/ReSwift/ReSwift.git", from: "6.0.0"),
-//.package(url: "https://github.com/matomo-org/matomo-sdk-ios.git", from: "7.4.1"),
-//.package(url: "https://github.com/onevcat/Kingfisher.git", from: "6.1.1"),
---
+++ Sources/MySabaySdkDependencies/MSWelcome.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Lay Channara on 3/30/21.
+//
+
+import Foundation
+
+public struct MSWelcome {
+	var hello: String = "Hello MySabaySdk!"
+}
